86497532bbc75c072e9665a607f65cf3009edd1a
Matt Tognetti <matt@mjtognetti.com>
2024-10-24 14:07:31 -0700

    basic application fields

--- lib/discord/application.rb
+++ lib/discord/application.rb
@@ -1,4 +1,8 @@
 module Discord
   class Application < Resource
+    summary "Applications (or \"apps\") are containers for developer platform features, and can be installed to Discord servers and/or user accounts."
+
+    field :id,   :snowflake, description: "ID of the app"
+    field :name, :string,    description: "Name of the app"
   end
 end
